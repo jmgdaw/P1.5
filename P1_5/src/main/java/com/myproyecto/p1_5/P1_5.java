cf3c088ba88cded1a87d15cd90154d88a7a78698
Kiru <jmgdaw8@gmail.com>
2024-01-10 10:40:34 +0100

    commit añadir p1.5 java

---
+++ P1_5/src/main/java/com/myproyecto/p1_5/P1_5.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.myproyecto.p1_5;
+
+/**
+ *
+ * @author Kiru
+ */
+public class P1_5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("adios");
+    }
+}
